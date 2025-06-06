5e639e0e662b9f35efd8cb3dbe14ef9e90ee895e
UZB02 <muhsinbekmirzamatov3@gmail.com>
2025-06-06 18:05:12 +0500

    server js da xatolik o'zgartirildi

--- server.js
+++ server.js
@@ -1,21 +1,10 @@
 // server.js
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const app = require("./app");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config();
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
-
-  // mongoose
-  //   .connect(process.env.MONGO_URI, {
-  //     useNewUrlParser: true,
-  //     useUnifiedTopology: true,
-  //   })
-  //   .then(() => console.log("MongoDB connected"))
-  //   .catch((err) => console.error(err));
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  console.log('MongoDB ulandi');
+  app.listen(5000, () => console.log('Server ishga tushdi: 5000-portda'));
+});
