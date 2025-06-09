634d8d909dc099e6dab5a7a19ff63eb3b7fd4ca3
UZB02 <muhsinbekmirzamatov3@gmail.com>
2025-06-09 10:29:06 +0500

    mongodab url yangilandi

--- config/conectDB.js
+++ config/conectDB.js
@@ -1,7 +1,7 @@
 // server.js
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const app = require('./app');
+const app = require('..');
 
 dotenv.config();
 mongoose.connect(process.env.MONGODB_URI).then(() => {
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
 
 const questionRoutes = require("./routes/questionRoutes");
 const adminRoutes = require("./routes/adminRoutes");
-
+dotenv.config();
 app.use(express.json());
 
 // CORS ni barcha domenlar uchun, credentials bilan ruxsat berish
@@ -20,5 +22,10 @@ app.use(
 // Routerlar
 app.use("/api/questions", questionRoutes);
 app.use("/api/admin", adminRoutes);
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  console.log("MongoDB ulandi");
+  app.listen(5000, () => console.log("Server ishga tushdi: 5000-portda"));
+});
 
 module.exports = app;
